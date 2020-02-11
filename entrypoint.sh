af6e2d1c8d0afbbfe7bd350e405b196af2e7563f
Aaron Roller <aaron.roller@automodality.com>
2020-02-11 13:14:25 -0800

    cleaning the entire workspace if in actions BB-789/clean-up

--- entrypoint.sh
+++ entrypoint.sh
@@ -139,9 +139,10 @@ mv ../$artifact_filename $artifact_path
 ls -lh $artifact_path
 
 # cleanup for the next user - BB-789 ARM builds fails since root owns generated directories.
-echo deleting generated directories debian and catkin_ws
-rm -rf debian
-rm -rf catkin_ws
+if [[ "$GITHUB_ACTIONS" == "true" ]]; then
+    echo deleting workspace for next build
+    rm -rf *
+fi
 
 echo ::set-output name=artifact-path::$artifact_path  #reference available to other actions
 
