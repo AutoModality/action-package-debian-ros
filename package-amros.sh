245ba7ca76d2546224b743e1295a7a1b816f1ee5
Aaron Roller <aaron.roller@automodality.com>
2020-09-27 08:38:08 -0700

    skipping installation for now AM-498/amros-docker

--- package-amros.sh
+++ package-amros.sh
@@ -13,6 +13,6 @@ branch=${4:-$NONE}
 
 # authenticate as sudo to allow installation without prompt
 # requires password = username
-echo amros | sudo -S whoami
-amros install -y
+# echo amros | sudo -S whoami
+# amros install -y
 /package.sh "$version" "$build_number" "$pull_request_number" "$branch"
